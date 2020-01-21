5cf5558671c746066244981a1fcbae9f44481a7e
xfangcosmo <fang.307@osu.edu>
2020-01-21 15:03:46 -0700

    xfang cov test

--- compute_covariances_fourier.c
+++ compute_covariances_fourier.c
@@ -810,8 +810,8 @@ int main(int argc, char** argv)
     sprintf(survey.name,"%s_area%le_ng%le_nl%le",survey_designation[t],survey.area,survey.n_gal,survey.n_lens);
     printf("area: %le n_source: %le n_lens: %le\n",survey.area,survey.n_gal,survey.n_lens);
 
-    sprintf(covparams.outdir,"/home/u17/timeifler/covparallel/"); 
-    // sprintf(covparams.outdir,"out_cov_lsstxso/");
+    // sprintf(covparams.outdir,"/home/u17/timeifler/covparallel/"); 
+    sprintf(covparams.outdir,"out_cov_lsstxso/");
     //sprintf(covparams.outdir,"/halo_nobackup/cosmos/teifler/covparallel/");
 
     printf("----------------------------------\n");  
