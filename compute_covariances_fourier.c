e87543cb70ceb0061d771e20e5bf1002bed9642b
xfangcosmo <fang.307@osu.edu>
2020-08-31 18:11:12 -0700

    recompute some blocks

--- compute_covariances_fourier.c
+++ compute_covariances_fourier.c
@@ -159,7 +159,7 @@ void run_cov_ll_ls(char *OUTFILE, char *PATH, double *ell, double *dell, int n1,
       c_ng = 0.; c_g = 0.;
       weight = test_kmax(ell[nl1],z1)*test_kmax(ell[nl2],zl);
       if (weight){
-        if((z1 == zl) && covparams.ng){c_ng = cov_NG_cl_gl_tomo(ell[nl1],ell[nl2],z1,z2,zl,zs);}
+        if((z1 == zl)){c_ng = cov_NG_cl_gl_tomo(ell[nl1],ell[nl2],z1,z2,zl,zs);}
         if (nl1 == nl2){
           c_g =  cov_G_cl_gl_tomo(ell[nl1],dell[nl1],z1,z2,zl,zs);
         }
@@ -188,7 +188,7 @@ void run_cov_ll_ll(char *OUTFILE, char *PATH, double *ell, double *dell, int n1,
     for (nl2 = 0; nl2 < like.Ncl; nl2 ++){
       c_ng = 0.; c_g = 0.;
       weight = test_kmax(ell[nl1],z1)*test_kmax(ell[nl2],z3);
-      if ((z1 == z3) && weight && covparams.ng) {
+      if ((z1 == z3) && weight) {
         c_ng = cov_NG_cl_cl_tomo(ell[nl1],ell[nl2],z1,z2,z3,z4);
       }
       if (nl1 == nl2){
@@ -728,7 +728,7 @@ void run_cov_ll_lk(char *OUTFILE, char *PATH, double *ell, double *dell, int n1,
       c_ng = 0.; c_g = 0.;
       weight = test_kmax(ell[nl1],z1)*test_kmax(ell[nl2],zl);
       if (weight){
-        if((z1 == zl) && covparams.ng) {c_ng = cov_NG_gg_gk(ell[nl1],ell[nl2],z1,z2,zl);}
+        if((z1 == zl)) {c_ng = cov_NG_gg_gk(ell[nl1],ell[nl2],z1,z2,zl);}
         if (nl1 == nl2){
           c_g =  cov_G_gg_gk(ell[nl1],dell[nl1],z1,z2,zl);
         }
--- xfang_sub.py
+++ xfang_sub.py
@@ -2,7 +2,7 @@ import numpy as np
 from subprocess import call
 import sys
 
-def get_script(i,j):
+def get_script(i,j, scenario):
 	script = """
 #!/bin/bash
 #PBS -S /bin/bash
@@ -12,7 +12,7 @@ def get_script(i,j):
 #PBS -J %d-%d
 #PBS -l select=1:ncpus=1:mem=1GB
 #PBS -l place=pack:shared
-#PBS -l walltime=7:00:00
+#PBS -l walltime=5:00:00
 #PBS -N LSSTxSO_cov
 #PBS -e /home/u1/xfang/output/
 #PBS -o /home/u1/xfang/output/
@@ -20,16 +20,18 @@ def get_script(i,j):
 module load gsl/2/2.1
 
 cd $PBS_O_WORKDIR
-/home/u1/xfang/CosmoLike/LSSTxSO/./compute_covariances_fourier $PBS_ARRAY_INDEX >&/home/u1/xfang/output/job_output_$PBS_ARRAY_INDEX.log
-	"""%(i,j)
+/home/u1/xfang/CosmoLike/LSSTxSO/./compute_covariances_fourier $PBS_ARRAY_INDEX %d >&/home/u1/xfang/output/job_output_$PBS_ARRAY_INDEX.log
+	"""%(i,j, scenario)
 	return script
 
 
 i_in = np.int(sys.argv[1])
 i_fi = np.int(sys.argv[2])
+scenario = np.int(sys.argv[3])
 
 print(i_in,i_fi)
-script = get_script(i_in, i_fi)
+print('scenario: %d'%(scenario))
+script = get_script(i_in, i_fi, scenario)
 print(script)
 
 f = open("xfang_cov.txt","w")
