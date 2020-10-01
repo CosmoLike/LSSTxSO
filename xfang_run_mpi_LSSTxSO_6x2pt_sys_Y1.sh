fd74bddab73da54dfa8cf87cac12d1a0db5d12b9
xfangcosmo <fang.307@osu.edu>
2020-10-01 00:49:21 -0700

    fix scripts

--- xfang_run_mpi_LSSTxSO_6x2pt_sys_Y1.sh
+++ xfang_run_mpi_LSSTxSO_6x2pt_sys_Y1.sh
@@ -7,8 +7,8 @@
 ### mem=168gb is memory per node, pcmem=6gb is the memory per core - optional
 #PBS -l select=40:ncpus=28:mem=168GB
 #PBS -l place=free:shared
-#PBS -l cput=11200:00:00
-#PBS -l walltime=8:00:00
+#PBS -l cput=23000:00:00
+#PBS -l walltime=20:00:00
 #PBS -N lsstso1_6x2
 #PBS -e /home/u1/xfang/output/
 #PBS -o /home/u1/xfang/output/
