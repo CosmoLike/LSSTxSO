df50bb16d75e7110ae8c220495c070289c8d5488
xfangcosmo <fang.307@osu.edu>
2020-11-29 22:58:52 -0800

    change cput

--- xfang_run_mpi_WFIRSTxSO_3x2pt_sys.sh
+++ xfang_run_mpi_WFIRSTxSO_3x2pt_sys.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #PBS -S /bin/bash
 #PBS -W group_list=cosmo
-#PBS -q standard
+#PBS -q high_pri
 ### Set the number of nodes,cores and memory that will be used for this job
 ### select=3 is the node count, ncpus=28 are the cores in each node,
 ### mem=168gb is memory per node, pcmem=6gb is the memory per core - optional
 #PBS -l select=40:ncpus=28:mem=168GB
 #PBS -l place=free:shared
-#PBS -l cput=28300:00:00
-#PBS -l walltime=25:00:00
+#PBS -l cput=20000:00:00
+#PBS -l walltime=17:00:00
 #PBS -N wfirst-wide3x2
 #PBS -e /home/u1/xfang/output/
 #PBS -o /home/u1/xfang/output/
