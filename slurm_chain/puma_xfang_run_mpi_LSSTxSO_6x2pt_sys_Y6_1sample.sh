0dd479da34838a329e7002b1b391131f45f41418
xfangcosmo <fang.307@osu.edu>
2021-03-23 21:17:25 -0700

    add 1sample slurm job

---
+++ slurm_chain/puma_xfang_run_mpi_LSSTxSO_6x2pt_sys_Y6_1sample.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=lsstso6_6x2
+#SBATCH --ntasks=1128
+#SBATCH --ntasks-per-node=94
+#SBATCH --nodes=12
+#SBATCH --mem-per-cpu=1gb
+#SBATCH --time=30:00:00
+
+#SBATCH --account=cosmo
+#SBATCH --partition=standard
+#SBATCH --qos=user_qos_timeifler
+#SBATCH --output=/home/u1/xfang/output/%A.out
+#SBATCH --error=/home/u1/xfang/output/%A.err
+
+cd $SLURM_SUBMIT_DIR
+
+module load gsl/2.6
+module load python/3.6/3.6.5
+module load openmpi3/3.1.4
+
+### run your executable program with begin and end date and time output
+# export MPI_DSM_DISTRIBUTE
+date
+/usr/bin/time mpirun -n 1128 python xfang_runLSSTxSO_6x2pt_sys_Y6_1sample.py
+date
