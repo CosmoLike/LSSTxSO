5bfd04e89fe4a4d36318e156426714b2ce636fad
Xiao Fang <fx910525@gmail.com>
2021-04-20 16:36:12 -0700

    add 3x2pt y1

---
+++ slurm_chain/puma_xfang_run_mpi_LSSTxSO_3x2pt_sys_Y1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=lsst1_3x2
+#SBATCH --nodes=12
+#SBATCH --ntasks=1128
+#SBATCH --ntasks-per-node=94
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1gb
+#SBATCH --time=30:00:00
+
+#SBATCH --account=cosmo
+#SBATCH --partition=standard
+#SBATCH --qos=user_qos_timeifler
+#SBATCH --output=%A.out
+#SBATCH --error=%A.err
+
+cd $SLURM_SUBMIT_DIR
+
+module load gsl/2.6
+module load python/3.6/3.6.5
+module load openmpi3/3.1.4
+
+### run your executable program with begin and end date and time output
+export MPI_DSM_DISTRIBUTE
+date
+/usr/bin/time mpirun --mca pml ob1 --mca btl ^openib -n 1128 python3 xfang_runLSSTxSO_3x2pt_sys_Y1.py 1128
+date
