ef38ffb81c260853a6704f90cabcce7b8f53a539
xfangcosmo <fang.307@osu.edu>
2021-05-29 23:46:26 -0700

    edit y6 6x2pt script ocelote

--- pbs_chain/xfang_run_mpi_LSSTxSO_6x2pt_sys_Y6.sh
+++ pbs_chain/xfang_run_mpi_LSSTxSO_6x2pt_sys_Y6.sh
@@ -7,8 +7,8 @@
 ### mem=168gb is memory per node, pcmem=6gb is the memory per core - optional
 #PBS -l select=40:ncpus=28:mem=168GB
 #PBS -l place=free:shared
-#PBS -l cput=23000:00:00
-#PBS -l walltime=20:00:00
+#PBS -l cput=30240:00:00
+#PBS -l walltime=27:00:00
 #PBS -N lsstso6_6x2
 #PBS -e /home/u1/xfang/output/
 #PBS -o /home/u1/xfang/output/
