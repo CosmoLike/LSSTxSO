23bb5c54d98bdca1d18ccc0f7ff26623bbdda5a2
xfangcosmo <fang.307@osu.edu>
2021-06-27 13:10:39 -0700

    add scripts for outlier 1sample chains

--- slurm_chain/puma_xfang_run_mpi_LSSTxSO_6x2pt_sys_Y6_outlier_1sample.sh
+++ slurm_chain/puma_xfang_run_mpi_LSSTxSO_6x2pt_sys_Y6_outlier_1sample.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=94
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1gb
-#SBATCH --time=26:00:00
+#SBATCH --time=22:30:00
 
 #SBATCH --account=cosmo
 #SBATCH --partition=standard
